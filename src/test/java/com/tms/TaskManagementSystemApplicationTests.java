9201a71012ce939cb4f63d26d7ba912deec32145
OD <sdenisenko055@gmail.com>
2024-12-13 16:40:05 +0200

    TMS foundation

---
+++ src/test/java/com/tms/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TaskManagementSystemApplication.class)
+public class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
